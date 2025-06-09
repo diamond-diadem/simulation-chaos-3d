9e41df475587b39f25332b16677659e3aa8375da
David Martin-Calle <david.martin-calle@outlook.fr>
2025-06-09 13:21:16 +0200

    Remove legacy Lorenz and Rossler simulation classes and their dependencies

--- machinery-old/__init__.py
+++
@@ -1,8 +0,0 @@
-# machinery/__init__.py
-
-from .lorenz import LorenzSystem, LorenzSimulation
-from .rossler import RosslerSystem, RosslerSimulation
-
-__all__ = ["LorenzSystem", "LorenzSimulation", 
-           "RosslerSystem", "RosslerSimulation"]
-
