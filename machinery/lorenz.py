22321ef2b553061ca01fc40d47f2abe2bd7c4761
David Martin-Calle <david.martin-calle@outlook.fr>
2025-06-09 11:51:40 +0200

    Add 'backend' parameter to plot_solution method. 'matplotlib' or 'plotly'

--- machinery/lorenz.py
+++ machinery/lorenz.py
@@ -1,6 +1,5 @@
 import numpy as np
 from scipy.integrate import solve_ivp
-import plotly.graph_objects as go
 
 class LorenzSystem:
     def __init__(self, sigma, rho, beta):
@@ -49,20 +48,39 @@ class LorenzSimulation:
         if self.solution:
             np.savez(path, t=self.solution.t, X=self.solution.y)
 
-    def plot_solution(self):
+    def plot_solution(self, backend='matplotlib'):
 
+        assert backend in ['matplotlib', 'plotly'], "backend must be 'plotly' or 'matplotlib'"
+        
         if self.solution is None:
-            raise ValueError("No solution available. Please run the simulation first.")
-
+                raise ValueError("No solution available. Please run the simulation first.")
+        
         x, y, z = self.solution.y
-        fig = go.Figure(data=go.Scatter3d(
-            x=x, y=y, z=z,
-            mode='lines',
-            line=dict(width=1, color='blue')
-        ))
-        fig.update_layout(scene=dict(
-            xaxis_title='x',
-            yaxis_title='y',
-            zaxis_title='z'
-        ))
-        fig.show()
+        
+        if backend == 'matplotlib':
+
+            import matplotlib.pyplot as plt
+            from mpl_toolkits.mplot3d import Axes3D
+            
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection='3d')
+            ax.plot(x, y, z, lw=1, color='blue')
+            ax.set_xlabel('x')
+            ax.set_ylabel('y')
+            ax.set_zlabel('z')
+            plt.show()
+        
+        elif backend == 'plotly':
+            
+            import plotly.graph_objects as go
+            fig = go.Figure(data=go.Scatter3d(
+                x=x, y=y, z=z,
+                mode='lines',
+                line=dict(width=1, color='blue')
+            ))
+            fig.update_layout(scene=dict(
+                xaxis_title='x',
+                yaxis_title='y',
+                zaxis_title='z'
+            ))
+            fig.show()
